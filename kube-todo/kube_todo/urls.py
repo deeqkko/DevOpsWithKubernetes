6336d8ec060653dc087ae5af97a8dd8ff85cbcbf
deeqkko <kimmo1706@student.hamk.fi>
2021-04-06 11:49:34 +0300

    reverted to original

--- kube-todo/kube_todo/urls.py
+++ kube-todo/kube_todo/urls.py
@@ -21,8 +21,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
-    path('kube-todo/admin/', admin.site.urls),
-    path('kube-todo/', include('backend.urls'))
+    path('admin/', admin.site.urls),
+    path('', include('backend.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
